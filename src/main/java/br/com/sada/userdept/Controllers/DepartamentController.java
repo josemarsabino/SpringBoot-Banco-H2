2be794aba50121c91345671a40e5b385fcfdca5b
josemarsabino <josemarsabino@hotmail.com>
2022-01-01 18:26:07 -0300

    Metodo de Gravacao de um Departamento

--- src/main/java/br/com/sada/userdept/Controllers/DepartamentController.java
+++ src/main/java/br/com/sada/userdept/Controllers/DepartamentController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +30,11 @@ public class DepartamentController {
 		Department result = departRepository.findById(id).get();
 		return result;
 	}
-	
-	
+
+	@PostMapping
+	public Department inclusao(@RequestBody Department entity) {
+		Department result = departRepository.save(entity);
+		return result;
+	}
 
 }
