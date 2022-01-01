174ab7323111d245e8be812d9939e7f01b9d08b1
josemarsabino <josemarsabino@hotmail.com>
2022-01-01 17:56:38 -0300

    Criando Controller Departament Listar Todos

--- src/main/java/br/com/sada/userdept/Controllers/DepartamentController.java
+++ src/main/java/br/com/sada/userdept/Controllers/DepartamentController.java
@@ -1,10 +1,24 @@
 package br.com.sada.userdept.Controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.sada.userdept.model.Department;
+import br.com.sada.userdept.repository.DepartmentRepository;
+
 @RestController
-@RequestMapping(value="/departments")
+@RequestMapping(value = "/departments")
 public class DepartamentController {
+
+	@Autowired
+	DepartmentRepository departRepository;
+
+	public List<Department> listarTodos() {
+		List<Department> result = departRepository.findAll();
+		return result;
+	}
 
 }
